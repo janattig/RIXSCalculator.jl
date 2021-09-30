19a1fb8ed28faaaaa2fe0325d6e81cb9467f9922
lpeterli <lpeterli@thp.uni-koeln.de>
2021-09-30 11:42:37 +0200

    added transitions.jl file

---
+++ src/transitions/transitions.jl
@@ -0,0 +1,24 @@
+##############################################################
+#
+#   TRANSITIONS
+#
+##############################################################
+
+
+
+
+# DEFINITION OF DIPOLE OPERATOR
+include("transitions_dipole_operator.jl")
+
+
+# DEFINITION OF SPECTRUM
+include("transitions_spectrum.jl")
+
+# PLOTTING OF SPECTRUM
+include("transitions_spectrum_plotting.jl")
+
+
+
+
+# LAB SYSTEM
+include("transitions_lab_system.jl")
