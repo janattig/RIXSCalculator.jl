20aa300d1b698992cd20ac4ce1f20cc6c533ab00
Luca Peterlini <lpeterli@l24.thp.uni-koeln.de>
2021-10-11 10:20:58 +0200

    moved overlap documentation

--- src/basis/basis_sp/basisstate_abstract_type_sp.jl
+++ src/basis/basis_sp/basisstate_abstract_type_sp.jl
@@ -5,14 +5,6 @@ export  AbstractSPBasisState
 
 
 # general overlap function for any single particle states
-"""
-    overlap(state_1 :: SPSSBS, state_2 :: SPSSBS) :: Complex{Float64}   where {SPSSBS <: AbstractSPSSBasisState}
-
-This function computes the term ``\\left< state_1 | state_2\\right>``.
-
-A complex number is returned.
-
-"""
 function overlap(state_1 :: BS1, state_2 :: BS2) :: Complex{Float64} where {BS1<:AbstractSPBasisState, BS2<:AbstractSPBasisState}
     @error "not implemented function 'overlap' for single particle basis states of types "*string(BS1)*" and "*string(BS2) stacktrace()
     return NaN+ NaN*im
--- src/basis/basisstate_abstract_type.jl
+++ src/basis/basisstate_abstract_type.jl
@@ -10,6 +10,14 @@ export AbstractBasisState
 
 
 # general abstract overlap function
+"""
+    overlap(state_1 :: BS1, state_2 :: BS2) :: Complex{Float64} where {BS1<:AbstractBasisState, BS2<:AbstractBasisState}
+
+This function computes the term ``\\left< state_1 | state_2\\right>``.
+
+A complex number is returned.
+
+"""
 function overlap(state_1 :: BS1, state_2 :: BS2) :: Complex{Float64} where {BS1<:AbstractBasisState, BS2<:AbstractBasisState}
     @error "not implemented abstract function 'overlap' for basis states of types "*string(BS1)*" and "*string(BS2) stacktrace()
     return NaN+ NaN*im
