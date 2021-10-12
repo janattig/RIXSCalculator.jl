aa98d6cc5b0781cbb8ebaf66cc7875b4731a5474
Luca Peterlini <lpeterli@l24.thp.uni-koeln.de>
2021-10-12 10:55:04 +0200

    added LabSystem.jl

--- src/lab_system/LabSystem.jl
+++ src/lab_system/LabSystem.jl
@@ -4,6 +4,7 @@ include("LabSystem_type.jl")
 # include LabSystem functions
 include("LabSystem_functions/function_miscellaneous.jl")
 include("LabSystem_functions/scattering_settings.jl")
+include("LabSystem_functions/spin_quantization.jl")
 
-
-include("LabSystem_geometry.jl")
+# include lab geometry
+include("lab_geometry.jl")
