541d6ef7cc79ce119eeb4135e2b1fb82ab23602c
lpeterli <lpeterli@thp.uni-koeln.de>
2021-10-18 16:22:08 +0200

    test on runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,18 +5,18 @@ using Pkg
 @testset "RIXSCalculator.jl" begin
 
     # include subfile for abstract type tests
-    include("test_type_trees.jl")
+#     include("test_type_trees.jl")
 
-    # include subfile for constructor tests
-    include("test_constructors.jl")
+#     # include subfile for constructor tests
+#     include("test_constructors.jl")
 
-    # include subfile for interface function tests
-    include("test_interface_functions.jl")
+#     # include subfile for interface function tests
+#     include("test_interface_functions.jl")
     
-    # include subfile for all other function tests
-    include("test_functions.jl")
+#     # include subfile for all other function tests
+#     include("test_functions.jl")
     
-    # include subfile for physics tests
-    include("test_physics.jl")
+#     # include subfile for physics tests
+#     include("test_physics.jl")
 
 end
