9844a4485aef2776d36d9a21987dcdae18d9995c
lpeterli <lpeterli@thp.uni-koeln.de>
2021-10-19 22:07:03 +0200

    Created file structure

---
+++ test/test_physics.jl
@@ -0,0 +1,26 @@
+# TEST FOR THE PHYSICS BEHIND THE CODE
+# it mainly tests eigenenergies with specific hamiltonians and consistency in various instances
+
+@testset "Physics tests" begin
+    
+    @testset "Eigenvalue tests" begin
+    
+    
+    end
+
+
+    @testset "Eigenvector tests" begin
+        
+        
+        
+    end
+    
+    @testset "Literature tests" begin
+        
+        
+        
+    end
+    
+    
+    
+end #end physics tests
