a632c0462f3532d6d801ad60e7cdd5cafcde122a
Luca Peterlini <lpeterli@l24.thp.uni-koeln.de>
2021-10-20 11:00:06 +0200

    added first test: 1s1h magnetic field

--- test/test_physics.jl
+++ test/test_physics.jl
@@ -1,10 +1,60 @@
 # TEST FOR THE PHYSICS BEHIND THE CODE
 # it mainly tests eigenenergies with specific hamiltonians and consistency in various instances
 
+# Auxiliary functions:
+"""
+    generate_rvos()
+
+Generates a Random Vector On a Sphere.
+"""
+function generate_rvos()
+    theta=rand()*2*pi    #rand(Uniform(0.0,2*pi))
+    z=-1.0+2*rand()    #rand(Uniform(0.0,1.0))
+    
+    x=sqrt(1-z^2)*cos(theta)
+    y=sqrt(1-z^2)*sin(theta)
+    
+    return [x,y,z]
+end
+
+
+
 @testset "Physics tests" begin
     
     @testset "Eigenvalue tests" begin
     
+        @testset "BdotS only" begin
+            
+            @testset "1h, 1s" begin
+               
+                h=2
+                s=1
+                Bstr=1.0
+                Bdir=generate_rvos()
+                B=Bstr*Bdir
+                
+                # basis construction
+                basis_sp=getT2GBasisLS()        
+                basis_ms=getMultiSiteBasis(basis_sp,s)
+                basis_mp=getMultiParticleBasis(basis_ms,h)
+                
+                # hamiltonian construction
+                hamiltonian=MagneticFieldOperator(basis_mp, 1, Bstr, Bdir)
+                
+                # obtain eigensystem
+                es=eigensystem(hamiltonian)
+                
+                # correct results:
+                Epm=norm(B)/2
+                E1=-Epm
+                E2=Epm
+                
+                #test
+                @test (es[:values]-[E1,E1,E1,E2,E2,E2])<1e-6*ones(6)
+                
+            end
+            
+        end
     
     end
 
