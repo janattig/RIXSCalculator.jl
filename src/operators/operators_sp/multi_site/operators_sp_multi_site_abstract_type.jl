72ffad6f1f44a0d3f218280efcfeaa8652849702
Luca Peterlini <lpeterli@smail.uni-koeln.de>
2021-11-26 16:56:33 +0100

    modified AbstractSMMSOperator type to work with DelocalizedBasisStateXYZ

--- src/operators/operators_sp/multi_site/operators_sp_multi_site_abstract_type.jl
+++ src/operators/operators_sp/multi_site/operators_sp_multi_site_abstract_type.jl
@@ -1,6 +1,6 @@
 # abstract operator super type to all SP MS operators
 abstract type AbstractSPMSOperator{
-        SPB <: SPBasis{SPMSBS} where {SPMSBS<:Union{SPMSBasisState{SPSSBS} where SPSSBS, SPMSCompositeBasisState{B} where B}}
+        SPB <: SPBasis{SPMSBS} where {SPMSBS<:Union{SPMSBasisState{SPSSBS} where SPSSBS, SPMSCompositeBasisState{B} where B, DelocalizedBasisStateXYZ}}
 } <: AbstractSPOperator{SPB} end
 export AbstractSPMSOperator
 
