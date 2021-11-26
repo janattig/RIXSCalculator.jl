6628833534cabf496c273a5ccb88c278274d1668
Luca Peterlini <lpeterli@smail.uni-koeln.de>
2021-11-26 17:20:51 +0100

    Improved input type definition for add_hopping!

--- src/operators/specific_operators/hopping.jl
+++ src/operators/specific_operators/hopping.jl
@@ -289,13 +289,13 @@ end
 
 # add_hopping! for DelocalizedBasisState
 
-function add_hopping!(operator :: SPMSProjectorOperator{SPMSB_IN, SPMSB_OUT, SPO}, args...) where {SPMSBS <: DelocalizedBasisStateXYZ, SPMSB_IN<: SPBasis{SPMSBS}, SPMSB_OUT<: SPBasis{SPMSBS}, SPO}
+function add_hopping!(operator :: SPMSProjectorOperator{SPMSB_IN, SPMSB_OUT, SPO}, args...) where {SPSSBS <: AbstractSPSSBasisState, SPMSBS <: Union{SPMSBasisState{SPSSBS}, DelocalizedBasisStateXYZ}, SPMSB_IN<: SPBasis{SPMSBS}, SPMSB_OUT<: SPBasis{SPMSBS}, SPO}
     # pass further into the hopping operator
     add_hopping!(operator.operator, args...)
     # return nothing
     return nothing
 end
-function add_diagonal_hopping!(operator :: SPMSProjectorOperator{SPMSB_IN, SPMSB_OUT, SPO}, args...) where {SPMSBS <: DelocalizedBasisStateXYZ, SPMSB_IN<: SPBasis{SPMSBS}, SPMSB_OUT<: SPBasis{SPMSBS}, SPO}
+function add_diagonal_hopping!(operator :: SPMSProjectorOperator{SPMSB_IN, SPMSB_OUT, SPO}, args...) where {SPSSBS <: AbstractSPSSBasisState, SPMSBS <: Union{SPMSBasisState{SPSSBS}, DelocalizedBasisStateXYZ}, SPMSB_IN<: SPBasis{SPMSBS}, SPMSB_OUT<: SPBasis{SPMSBS}, SPO}
     # pass further into the hopping operator
     add_diagonal_hopping!(operator.operator, args...)
     # return nothing
